564b693b0c0e25e1531ab61ba4045abe653d74a0
Dean Hunter <jeeka7@gmail.com>
2022-06-01 22:03:20 +0530

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -4,5 +4,9 @@ import pandas as pd
 scoredict = {
   "Player":0,"Computer":0,
 }
+
+if st.button("Rock")
+  st.write("Jeeka apna soney uthney ka schedule theek kar le")
+
 scoredf = pd.DataFrame(scoredict,index=[0])
 st.write(scoredf)
