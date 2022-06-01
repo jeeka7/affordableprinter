97fe116d71a99abccbf2cfed976fdf194206781f
Dean Hunter <jeeka7@gmail.com>
2022-06-01 21:28:59 +0530

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+scoredict = {
+  "Player":0,"Computer":0
+}
+scoredf = pd.DataFrame(scoredict)
 
 score = [["Player","Computer" ],[0,0]]
 
 st.write(score)
+st.write(scoredf)
+st.write(scoredict)
