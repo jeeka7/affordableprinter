1f11b6bc5153fefd19bed5a3ed840f9559cd81a7
Dean Hunter <jeeka7@gmail.com>
2022-06-01 21:29:52 +0530

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -4,10 +4,10 @@ import pandas as pd
 scoredict = {
   "Player":0,"Computer":0
 }
-scoredf = pd.DataFrame(scoredict)
+#scoredf = pd.DataFrame(scoredict)
 
 score = [["Player","Computer" ],[0,0]]
 
 st.write(score)
-st.write(scoredf)
+#st.write(scoredf)
 st.write(scoredict)
