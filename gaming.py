ef3756c2f7ab3f9e3b87a240fbc90b801b555cdf
Dean Hunter <jeeka7@gmail.com>
2022-06-01 21:55:46 +0530

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -5,5 +5,5 @@ scoredict = {
   "Player":0,"Computer":0,
 }
 scoredf = pd.DataFrame(scoredict,index=[0])
-scoredf.set_properties({'text-align': 'center'}).hide_index()
+scoredf.set_properties({'text-align': 'center'})
 st.write(scoredf)
