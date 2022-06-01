cd20a4a90a29b7a8dfbaeb36f43a68418543ab61
Dean Hunter <jeeka7@gmail.com>
2022-06-01 23:13:41 +0530

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -10,12 +10,19 @@ def listToString(s):
     str1 = " " 
     
     return (str1.join(s))
+def AddPlayer():
+ scoredict["Player"] += 1 
+def AddComputer():
+ scoredict["Computer"] += 1 
   
 if st.button("Rock"):
   Computerguess = random.sample(["Scissors","Paper"],1)
   st.write(type(listToString(Computerguess)))
   st.write(Computerguess)
   st.write('Computer used {}'.format(Computerguess))
-  if Computerguess
+  if Computerguess == "Scissors":
+    Addplayer()
+  else:
+    AddComputer()
 scoredf = pd.DataFrame(scoredict,index=[0])
 st.write(scoredf)
